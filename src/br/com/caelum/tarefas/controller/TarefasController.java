1963a2b7776505b31127ca2e4d6ffc049bddad54
web7188 <web7188@caelumBSB03-09>
2017-11-11 17:07:27 -0200

    Aula 4

--- src/br/com/caelum/tarefas/controller/TarefasController.java
+++ src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -1,6 +1,10 @@
 package br.com.caelum.tarefas.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.caelum.tarefas.dao.JdbcTarefaDao;
@@ -15,10 +19,20 @@ public class TarefasController {
 	}
 	
 	@RequestMapping("adicionaTarefa")
-	public String adiciona(Tarefa tarefa) {
+	public String adiciona(@Valid Tarefa tarefa, BindingResult result) {
+		if(result.hasFieldErrors("descricao")) {
+			return "tarefa/formulario";
+		}
+		
 		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.adiciona(tarefa);
 		return "tarefa/adicionada";
+	}
+	@RequestMapping("listaTarefas")
+	public String lista(Model model){
+		JdbcTarefaDao dao = new JdbcTarefaDao();
+		model.addAttribute("tarefas", dao.lista());
+		return "tarefa/lista";
 	}
 
 }
--- src/br/com/caelum/tarefas/modelo/Tarefa.java
+++ src/br/com/caelum/tarefas/modelo/Tarefa.java
@@ -2,8 +2,12 @@ package br.com.caelum.tarefas.modelo;
 
 import java.util.Calendar;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 public class Tarefa {
 	private Long id;
+	@NotNull(message="Escreva alguma coisa") @Size(min=5, max=255, message="Atenção no tamanho")
 	private String descricao;
 	private boolean finalizado;
 	private Calendar dataFinalizacao;
